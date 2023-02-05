84703af5db88e5ac84959222950016b7015d8921
Gurmigou <yehor.bukhanets@gmail.com>
2023-02-05 22:55:00 +0200

    Security and user service implemented

---
+++ backend/src/main/java/com/hackathon/backend/controller/security/SecurityController.java
@@ -0,0 +1,67 @@
+package com.hackathon.backend.controller.security;
+
+import com.hackathon.backend.dto.security.LoginRequestDto;
+import com.hackathon.backend.dto.security.RegistrationDto;
+import com.hackathon.backend.dto.user.UserDto;
+import com.hackathon.backend.model.User;
+import com.hackathon.backend.security.JwtProvider;
+import com.hackathon.backend.security.JwtUserDetailsService;
+import com.hackathon.backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hackathon/api/v1")
+public class SecurityController {
+    private final JwtUserDetailsService userDetailsService;
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+
+    @Autowired
+    public SecurityController(JwtUserDetailsService userDetailsService, JwtProvider jwtProvider,
+                              PasswordEncoder passwordEncoder, UserService userService) {
+        this.userDetailsService = userDetailsService;
+        this.jwtProvider = jwtProvider;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginRequestDto loginRequest) {
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getLogin());
+
+            if (!passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword()))
+                throw new BadCredentialsException("Password is incorrect");
+
+            String jwtToken = jwtProvider.generateToken(userDetails);
+            return ResponseEntity.ok(jwtToken);
+        } catch (BadCredentialsException | UsernameNotFoundException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Login or password is incorrect.");
+        }
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegistrationDto registrationDto) {
+        try {
+            UserDto newUser = userService.registerUser(registrationDto);
+            return ResponseEntity.ok(newUser);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+    }
+}
---
+++ backend/src/main/java/com/hackathon/backend/controller/user/UserController.java
@@ -0,0 +1,37 @@
+package com.hackathon.backend.controller.user;
+
+import com.hackathon.backend.dto.user.UserDto;
+import com.hackathon.backend.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/hackathon/api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * @param principal gets from Spring-security
+     * @return user dto
+     */
+    @GetMapping
+    public ResponseEntity<?> getUserInfo(Principal principal) {
+        try {
+            UserDto userInfo = userService.getUserDto(principal.getName());
+            return ResponseEntity.ok(userInfo);
+        } catch (Throwable t) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(t.getMessage());
+        }
+    }
+
+}
